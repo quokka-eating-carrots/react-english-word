9101d4a77df61b49baff85bc77f7e5e436de606e
quokka-eating-carrots <ovzip7@gmail.com>
2022-12-14 17:06:15 +0900

    6 이벤트 처리처

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -3,16 +3,26 @@ import World from './World'
 import styles from './Hello.module.css'
 
 function Hello() {
+  function showName() {
+    console.log('kiki')
+  }
+  function showAge(age) {
+    console.log(age)
+  }
+  function showText(txt) {
+    console.log(txt)
+  }
   return (
     <div>
-      <h1 style={{
-        color: '#f00',
-        borderRight: "2px solid #000",
-        marginBottom: "30px",
-        opacity: 0.5
-      }}>Hello</h1>
-      <World />
-      <div className={styles.box}>Hello</div>
+      <h1>Hello</h1>
+      <button onClick={showName}>Show name</button>
+      <button onClick={() => {
+        showAge(10)
+      }}>Show age</button>
+      <input type="text" onChange={e => {
+        const txt = e.target.value
+        showText(txt)
+      }} />
     </div>
   )
 }
