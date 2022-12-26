f5e5a3ca9ab1e798138ebda926b72a3cdc1a71af
quokka-eating-carrots <ovzip7@gmail.com>
2022-12-26 16:49:17 +0900

    Feat: 페이지 넘기기

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,15 +1,29 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import useFetch from '../hooks/useFecth'
 import Word from './Word'
 
 function Day() {
+  const days = useFetch("http://localhost:3001/days")
   const { day } = useParams()
+  const history = useNavigate()
   const words = useFetch(`http://localhost:3001/words?day=${day}`)
+
+  function prevPage() {
+    history(`/day/${+day === 1 ? 1 : +day - 1}`)
+  }
+
+  function nextPage() {
+    history(`/day/${+day === days.length ? days.length : +day + 1}`)
+  }
 
   return (
     <>
-      <h2>Day {day}</h2>
+      <div className="day">
+        <button onClick={prevPage}>◀️</button>
+        <h2>Day {day}</h2>
+        <button onClick={nextPage}>▶️</button>
+      </div>
       {words.length === 0 && <span>Loading...</span>}
       <table>
         <tbody>
